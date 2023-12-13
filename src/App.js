db938af803b649a46fb0d226271cc316d3da0c1f
enigm413 <rohithkkannikath@gmail.com>
2023-12-13 09:45:27 +0530

    Updated flash card component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+//Function to define Main App component
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FlashCards />
     </div>
   );
 }
 
-export default App;
+//Define Array of list of question
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
+
+function FlashCards() {
+  //Define state
+  const [selectedID, setSelectedID] = useState(null);
+
+  const handleClick = (id) => {
+    setSelectedID(id !== selectedID ? id : "");
+  };
+
+  return (
+    <div className="flashcards">
+      {questions.map((eachQuestion) => (
+        <div
+          onClick={() => handleClick(eachQuestion.id)}
+          key={eachQuestion.id}
+          className={eachQuestion.id === selectedID ? "selected" : ""}
+        >
+          <p>
+            {eachQuestion.id === selectedID
+              ? eachQuestion.answer
+              : eachQuestion.question}{" "}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
